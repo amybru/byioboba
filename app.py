281d262cbd397bac43562d16c313e32ed16c5677
amybru <amy.bru39@gmail.com>
2020-07-23 15:53:35 +0000

    began search feature

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ if path.exists('env.py'):
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
 
+
 mongo = PyMongo(app)
+
 
 # Home Page
 @app.route('/')
@@ -20,16 +22,27 @@ mongo = PyMongo(app)
 def home():
     return render_template("index.html")
 
+
 # Get Drinks: View all drinks in database with the option to filter
 @app.route('/get_drink')
 def get_drink():
     return render_template('drinks.html', boba=mongo.db.boba.find())
+
+
+# Search Drinks
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    boba = list(mongo.db.boba.find({"$text": {"$search": query}}))
+    return render_template("drinks.html", boba=boba)
+
 
 # Page to view one drink from the database
 @app.route('/see_one/<boba_id>')
 def see_one(boba_id):
     boba = mongo.db.boba.find_one({"_id": ObjectId(boba_id)})
     return render_template('components/drinkCard.html', boba=boba)
+
 
 # Add Drink form
 @app.route("/add_drink")
@@ -41,12 +54,14 @@ def add_drink():
     ice = mongo.db.ice.find()
     return render_template('addDrink.html', boba=mongo.db.boba.find(), drinks=list(drinks), teas=teas, toppings=toppings, sweet=sweet, ice=ice)
 
+
 # Function to post user data to the database
 @app.route('/insert_drink', methods=['POST'])
 def insert_drink():
     boba = mongo.db.boba
     boba.insert_one(request.form.to_dict())
     return redirect(url_for('get_drink'))
+
 
 # Edit Drink Form
 @app.route('/edit_drink/<boba_id>')
@@ -58,7 +73,8 @@ def edit_drink(boba_id):
     decaf = list(mongo.db.decaf.find())
     ice = list(mongo.db.ice.find())
     sweet = list(mongo.db.sweet.find())
-    return render_template('components/editDrink.html',  boba=boba, drinks=drinks, teas=teas, top=top, decaf=decaf, ice=ice, sweet=sweet)
+    return render_template('components/editDrink.html', boba=boba, drinks=drinks, teas=teas, top=top, decaf=decaf, ice=ice, sweet=sweet)
+
 
 # Update Drink in database
 @app.route('/update_drink/<boba_id>', methods=["POST"])
@@ -76,11 +92,13 @@ def update_drink(boba_id):
         })
     return redirect(url_for('get_drink'))
 
+
 # Delete Drink From Database
 @app.route('/delete_drink/<boba_id>')
 def delete_drink(boba_id):
     mongo.db.boba.remove({'_id': ObjectId(boba_id)})
     return redirect(url_for('get_drink'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
